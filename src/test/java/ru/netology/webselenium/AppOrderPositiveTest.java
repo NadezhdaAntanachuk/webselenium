87188efc55637bcba453fc3dbf0a38a2fe8c8437
NadezhdaAntanachuk <140953233+NadezhdaAntanachuk@users.noreply.github.com>
2024-03-31 02:02:25 +0300

    project updated 004

--- src/test/java/ru/netology/webselenium/AppOrderPositiveTest.java
+++ src/test/java/ru/netology/webselenium/AppOrderPositiveTest.java
@@ -40,24 +40,12 @@ public class AppOrderPositiveTest {
     }
 
     @Test
-    void shouldTestV1() {
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Василий");
-        elements.get(1).sendKeys("+79270000000");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("alert-success")).getText();
-        assertEquals("Ваша заявка успешно отправлена!", text.trim());
-    }
-
-    @Test
-    void shouldTestV2() {
-        WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
-        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
-        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
-        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        form.findElement(By.cssSelector("[data-test-id=submit]")).click();
-        String text = driver.findElement(By.className("alert-success")).getText();
-        assertEquals("Ваша заявка успешно отправлена!", text.trim());
+    void shouldBeSuccessfulForm() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Василий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79270000000");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("[data-test-id='submit']")).click();
+        var actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        assertEquals("Ваша заявка успешно отправлена!", actualText.trim());
     }
 }
