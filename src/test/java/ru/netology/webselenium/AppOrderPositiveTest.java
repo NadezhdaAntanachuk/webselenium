f2e8f534f0376dd8c81a8248fbaead0ced4f6ac5
NadezhdaAntanachuk <140953233+NadezhdaAntanachuk@users.noreply.github.com>
2024-03-31 02:47:32 +0300

    project updated 009

--- src/test/java/ru/netology/webselenium/AppOrderPositiveTest.java
+++ src/test/java/ru/netology/webselenium/AppOrderPositiveTest.java
@@ -1,17 +1,12 @@
 package ru.netology.webselenium;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -45,7 +40,7 @@ public class AppOrderPositiveTest {
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79270000000");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector("[data-test-id='submit']")).click();
-        var actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
-        assertEquals("Ваша заявка успешно отправлена!", actualText.trim());
+        var actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена!", actualText);
     }
 }
