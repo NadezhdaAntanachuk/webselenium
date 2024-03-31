36f0b089fc6886e5f552f943923a913d589f661d
NadezhdaAntanachuk <140953233+NadezhdaAntanachuk@users.noreply.github.com>
2024-03-31 14:15:57 +0300

    project updated 011

--- src/test/java/ru/netology/webselenium/AppOrderPositiveTest.java
+++ src/test/java/ru/netology/webselenium/AppOrderPositiveTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -39,8 +41,9 @@ public class AppOrderPositiveTest {
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Василий");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79270000000");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-        driver.findElement(By.cssSelector("[data-test-id='submit']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
         var actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
     }
+
 }
